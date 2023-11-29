c0fb5fcde5fc327152cec9e59fc3382926e92f7b
FranckyPapaTango <rafaralahyf@gmail.com>
2023-11-29 13:56:31 +0100

    first commit (base app backend server)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('rafaros-IT', 'computing', 'rafaralahyf@gmail.com'),
+  	('Madaskill', 'Backend server', 'madaskill.h2db@gmail.com'),
+  	('springboot', 'in-memory-db', 'madaskill-springboot.inmemory@gmail.com');
+  	
